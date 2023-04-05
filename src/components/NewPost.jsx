b638f1cf3eb5abef5a355057d7e7e7c1c0d7f745
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-04-05 15:19:24 -0400

    add NewPost form in PostList

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,15 @@
+import classes from "./NewPost.module.css"
+function NewPost() {
+    return (<form className={classes.form}>
+        <p>
+            <label htmlFor="body">Text</label>
+            <textarea id="body" required rows={3}/>
+        </p>
+        <p>
+            <label htmlFor="name">Your Name</label>
+            <input type="text" id="name" required></input>
+        </p>
+    </form>)
+}
+
+export default NewPost;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
 import classes from './PostList.module.css';
+import NewPost from "./NewPost";
 
 function PostList() {
 
-    return (<ul className={classes.posts}>
+    return (<>
+        <NewPost/>
+        <ul className={ classes.posts }>
          <Post author="Max" body="React is good"/>
         <Post author="Asha" body="I likke react now"/>
-    </ul>)
+        </ul>
+    </>)
     
 }
 
